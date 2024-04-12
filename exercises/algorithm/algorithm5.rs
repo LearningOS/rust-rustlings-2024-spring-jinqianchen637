86ea6b421d277a383ce8f7b56da9321852724e5e
chenjinqian <chenjinqian2012@163.com>
2024-04-12 18:09:12 +0800

    95

--- exercises/algorithm/algorithm5.rs
+++ exercises/algorithm/algorithm5.rs
@@ -3,8 +3,9 @@
 	This problem requires you to implement a basic BFS algorithm
 */
 
-//I AM NOT DONE
+//I AM DONE
 use std::collections::VecDeque;
+use std::collections::HashSet;
 
 // Define a graph
 struct Graph {
@@ -31,6 +32,30 @@ impl Graph {
 		//TODO
 
         let mut visit_order = vec![];
+        let mut visit_set = HashSet::new();
+        let mut task_vdq: VecDeque<usize> = VecDeque::new();
+        task_vdq.push_back(start);
+        let mut flag_not_finish = true;
+        while flag_not_finish {
+            let one_point = task_vdq.pop_front();
+            match one_point {
+                None => { 
+                    flag_not_finish = false;
+                    continue;
+                },
+                Some(point) => {
+                    if visit_set.contains(&point) {
+                        continue;
+                    }else {
+                        visit_set.insert(point);
+                    }
+                    visit_order.push(point);
+                    for sub_points in &self.adj[point] {
+                        task_vdq.push_back(*sub_points);
+                    }
+                }
+            }
+        }
         visit_order
     }
 }
